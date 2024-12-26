cf556a3f1c10489e8dd12c68eb93799b1e9cf8c1
student <student@DESKTOP-F93TTDI>
2024-12-26 10:51:28 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241226
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puppy MyPuppy = new Puppy();
+            //設定MyPuppy的屬性值.
+            MyPuppy.Id = 1;
+            MyPuppy.Owner = "Albee";
+            MyPuppy.Description ="cute";
+            MyPuppy.Gender = "famale";
+            MyPuppy.Variety = "薩摩耶";
+            MyPuppy.Age = 3;
+            MyPuppy.Food = 45000;
+            //類堆..所有屬性都要設.
+
+            //最後,叫用MyPuppy的方法PrintAllAttrib( )印出所有屬性參數.
+            MyPuppy.PrintAllAttrib();
+
+            //===============================================
+            //第二個 使用有參數的建構子設一隻寵物MickeyPuppy.
+            //經由建構子函式2 來設定所有屬性
+            Puppy MickeyPuppy = new Puppy(2, "Mickey", "8787", "female", "馬爾濟斯", 11, 50000);
+
+            //最後,叫用 MickeyPuppy的方法PrintAllAttrib( )印出所有屬性參數.
+            MickeyPuppy.PrintAllAttrib();
+        }
+    }
+}
---
+++ Puppy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241226
+{
+    internal class Puppy
+    {
+        //屬性
+        public int Id { get; set; }  //寵物的ID,編號
+        public string Owner { get; set; }  //寵物主人的名字
+        public string Description { get; set; } //關於這隻寵物的描述
+        public string Gender { get; set; } //性別
+        public string Variety { get; set; } //品種
+        public int Age { get; set; }  //年紀
+
+        private double _food; //伙食費
+        public double Food
+        {
+            get { return this._food; }
+            set { this._food = value; }
+        }
+
+        //建構子函式1
+        public Puppy() { }
+
+        //建構子函式2 
+        public Puppy(int id, string vOwner, string description, string gender, string variety, int age, double food)
+        {
+            Id = id;
+            Owner = vOwner;
+            Description = description;
+            Gender = gender;
+            Variety = variety;
+            Age = age;
+            Food = food;
+
+        }
+
+        public void PrintAllAttrib()
+        {
+            Console.WriteLine("ID is:" + this.Id.ToString());
+            Console.WriteLine("Owener is :" + this.Owner.ToString());
+            Console.WriteLine("Description is :" + this.Description.ToString());
+            Console.WriteLine("Gender is :" + this.Gender.ToString());
+            Console.WriteLine("Variety is :" + this.Variety.ToString());
+            Console.WriteLine("Age is :" + this.Age.ToString());
+            Console.WriteLine("Food is :" + this.Food.ToString());
+            //類推..把所有屬性印出來.
+
+        }
+    }
+}
